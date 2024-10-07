c506042c24235e6f17e75a7d5e5bb35147d956bb
faith wangari njau <faithjonnes91@gmail.com>
2024-10-07 15:34:08 +0300

    add text

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+
+// Declare customerName in global scope
+var customerName = 'bob';
+
+// Function to return the customerName
+function getCustomerName() {
+  return customerName;
+}
+
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+
+function setBestCustomer() {
+  var bestCustomer = 'not bob';
+  window.bestCustomer = bestCustomer;
+}
+
+
+function overwriteBestCustomer(newBestCustomer) {
+  window.bestCustomer = newBestCustomer;
+}
+
+
+function changeLeastFavoriteCustomer() {
+  const leastFavoriteCustomer = 'someone';
+  leastFavoriteCustomer = 'someone else'; 
+}
